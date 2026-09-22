cbcddd34d786e897456eae21840ad35fa2084409
kptattendance <kptattendance@gmail.com>
2026-09-22 17:35:04 +0530

    firs commit

---
+++ backend/src/server.js
@@ -0,0 +1,67 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://local.exams.kptmangaluru.in",
+  "https://exams.kptmangaluru.in",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+      "PATCH",
+      "OPTIONS",
+    ],
+
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
+
+    credentials: true,
+  })
+);
+
+app.use(clerkMiddleware());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "KPT Attendance & IA API is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+connectDB().then(() => {
+  if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  }
+});
+
+export default app;
